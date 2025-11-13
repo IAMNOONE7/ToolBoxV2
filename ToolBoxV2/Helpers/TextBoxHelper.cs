b7191973afe28f313aa4c4f6c5ee9f3a59784df4
IAMNOONE7 <v.hartan@seznam.cz>
2025-11-13 19:02:59 +0100

    Implemented Resources - themes

---
+++ ToolBoxV2/Helpers/TextBoxHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ToolBoxV2.Presentation.WPF.Helpers
+{
+    public static class TextBoxHelper
+    {
+        public static readonly DependencyProperty MeaningProperty =
+       DependencyProperty.RegisterAttached(
+           "Meaning", typeof(string), typeof(TextBoxHelper), new PropertyMetadata(string.Empty));
+
+        public static void SetMeaning(UIElement element, string value)
+        {
+            element.SetValue(MeaningProperty, value);
+        }
+
+        public static string GetMeaning(UIElement element)
+        {
+            return (string)element.GetValue(MeaningProperty);
+        }
+    }
+}
